c1046fbe9dba1d0fa4b6625789cb9291269ef4ab
sheikh ariful <sheikharifulislam770@gmail.com>
2022-12-10 02:32:50 +0600

    feat: The feature of how many students got which marks have been added

--- total_marks/total_marks.c
+++ total_marks/total_marks.c
@@ -8,14 +8,33 @@ int main() {
                         45, 67, 35, 28, 67, 34, 96, 43, 56, 34, 56, 97, 54, 42, 65, 85, 76, 94, 39, 67};
     int final_marks[40] = {45, 76, 93, 43, 25, 78, 93, 34, 90, 78, 56, 83, 56, 93, 93, 56, 49, 40, 39, 59,
                            20, 67, 87, 78, 34, 69, 30, 49, 89, 47, 89, 40, 74, 59, 37, 93, 89, 93, 45, 60};
-    double total_marks[40];
+    int total_marks[40];
+    int marks_count[101];
+    int i, j;
 
-    for (int i = 0; i < 40; i++) {
+    for (i = 0; i < 40; i++) {
         total_marks[i] = ft_marks[i] / 4.0 + st_marks[i] / 4.0 + final_marks[i] / 2.0;
     }
 
-    for (int j = 0; j < 40; j++) {
-        printf("Roll NO: %d\t Total Marks: %.2lf\n", j + 1, total_marks[j]);
+    for (j = 0; j < 40; j++) {
+        printf("Roll NO: %d\t Total Marks: %.d\n", j + 1, total_marks[j]);
+    }
+
+    for (i = 0; i <= 100; i++) {
+        marks_count[i] = 0;
+    }
+
+    for (i = 0; i < 40; i++) {
+        int mark = total_marks[i];
+        marks_count[mark]++;
+    }
+
+    printf("\n");
+
+    for (int mark = 20; mark <= 100; mark++) {
+        if (marks_count[mark] >= 1) {
+            printf("Mark: %d\t Count: %d\n", mark, marks_count[mark]);
+        }
     }
 
     return 0;
